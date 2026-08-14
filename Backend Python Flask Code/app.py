5da36b2be196125d61781d3be3bb2b1d7813ea39
Sonny Harrison <sonnyh.2188@gmail.com>
2026-08-14 17:16:44 +0100

    Updated app.py

--- Backend Python Flask Code/app.py
+++ Backend Python Flask Code/app.py
@@ -24,6 +24,7 @@ def main():
   length = 128
   email_domain = ""
   version = "UMTS VERSION 1.0"
+  PORT = os.environ.get("PORT)
 
   if request.method == "POST":
 
@@ -335,4 +336,4 @@ def main():
 # --------------------------------------------
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=20089, threaded=True)
+    app.run(host="0.0.0.0", port=PORT, threaded=True)
