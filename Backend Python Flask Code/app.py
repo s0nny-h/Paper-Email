d15c59efe2ef6cc60b4abcdb7abc2ce88868b7a5
Sonny Harrison <sonnyh.2188@gmail.com>
2026-08-17 09:02:50 +0100

    Update app.py
    Bug Fix

--- Backend Python Flask Code/app.py
+++ Backend Python Flask Code/app.py
@@ -38,8 +38,8 @@ def main():
   acc_sql_database = mysql.connector.connect(
     host=sql_host1,
     user=sql_username1,
-    password=sql_password1
-    #database=sql_database_1
+    password=sql_password1,
+    database=sql_database_1
   )
   
   #ema_sql_database = mysql.connector.connect(
